2654c59e9ee2c14535c51cf752b7fdb0965b0d04
qfu <tinyqf@gmail.com>
2015-06-11 15:48:17 +0800

    rm test code

--- TQEasyIconButton/TQEasyIconButton/ViewController.m
+++ TQEasyIconButton/TQEasyIconButton/ViewController.m
@@ -20,9 +20,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    [self.demoButton setTitle:@"Button" forState:UIControlStateNormal];
-    [self.demoButton setImage:@"icon" forState:UIControlStateNormal];
 }
 
 - (IBAction)left:(id)sender
